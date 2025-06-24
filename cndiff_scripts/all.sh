9474631ddf3dbfbfbd0c2079db2cd4891319b5a2
Omkar Kabde <omkarkabde@gmail.com>
2025-06-24 12:18:15 +0530

    all.sh

---
+++ cndiff_scripts/all.sh
@@ -0,0 +1,9 @@
+echo "X-X-X Classification X-X-X"
+bash /home/omkar/tslib/cndiff_scripts/classification.sh
+
+echo "X-X-X Anomaly Detection X-X-X"
+bash /home/omkar/tslib/cndiff_scripts/anomaly_detection.sh
+
+echo "X-X-X Imputation X-X-X"
+bash ./home/omkar/tslib/cndiff_scripts/imputation.sh
+
