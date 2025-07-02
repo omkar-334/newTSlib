3f3fe5f874e91aa89c5c7519ac4580a3961d4251
Omkar Kabde <omkarkabde@gmail.com>
2025-07-02 17:04:06 +0530

    imputation high

--- cndiff_scripts/imputation.sh
+++ cndiff_scripts/imputation.sh
@@ -19,75 +19,75 @@ timesteps=100
 # 100
 
 
-# for rate in 0.125 0.25 0.375 0.5
-# do
-#   python -u run.py \
-#     --task_name imputation \
-#     --is_training 1 \
-#     --root_path ./dataset/electricity/ \
-#     --data_path electricity.csv \
-#     --model_id ECL_mask_$rate \
-#     --mask_rate $rate \
-#     --model $model_name \
-#     --data custom \
-#     --gpu $device \
-#     --features M \
-#     --pred_len 96 \
-#     --c_out 321 \
-#     --d_model 96 \
-#     --attn_dropout $attn_dropout \
-#     --hidden_dim $hidden_dim \
-#     --mlp_ratio $mlp_ratio \
-#     --n_depth $n_depth \
-#     --n_emb $n_emb \
-#     --n_heads $n_heads \
-#     --timesteps $timesteps 
-# done
+for rate in 0.125 0.25 0.375 0.5
+do
+  python -u run.py \
+    --task_name imputation \
+    --is_training 1 \
+    --root_path ./dataset/electricity/ \
+    --data_path electricity.csv \
+    --model_id ECL_mask_$rate \
+    --mask_rate $rate \
+    --model $model_name \
+    --data custom \
+    --gpu $device \
+    --features M \
+    --pred_len 96 \
+    --c_out 321 \
+    --d_model 96 \
+    --attn_dropout $attn_dropout \
+    --hidden_dim $hidden_dim \
+    --mlp_ratio $mlp_ratio \
+    --n_depth $n_depth \
+    --n_emb $n_emb \
+    --n_heads $n_heads \
+    --timesteps $timesteps 
+done
 
-
-# for rate in 0.125 0.25 0.375 0.5
-# do
-#   python -u run.py \
-#     --task_name imputation \
-#     --is_training 1 \
-#     --root_path ./dataset/weather/ \
-#     --data_path weather.csv \
-#     --model_id weather_mask_$rate \
-#     --mask_rate $rate \
-#     --model $model_name \
-#     --data custom \
-#     --features M \
-#     --pred_len 96 \
-#     --c_out 21 \
-#     --gpu $device \
-#     --d_model 96 \
-#     --attn_dropout $attn_dropout \
-#     --hidden_dim $hidden_dim \
-#     --mlp_ratio $mlp_ratio \
-#     --n_depth $n_depth \
-#     --n_emb $n_emb \
-#     --n_heads $n_heads \
-#     --timesteps $timesteps 
-# done
-
-# ETTh1
 
 for rate in 0.125 0.25 0.375 0.5
 do
   python -u run.py \
     --task_name imputation \
     --is_training 1 \
-    --root_path ./dataset/ETT-small/ \
-    --data_path ETTh1.csv \
-    --model_id ETTh1_mask_$rate \
+    --root_path ./dataset/weather/ \
+    --data_path weather.csv \
+    --model_id weather_mask_$rate \
     --mask_rate $rate \
     --model $model_name \
     --data custom \
     --features M \
     --pred_len 96 \
-    --c_out 7 \
+    --c_out 21 \
     --gpu $device \
-    --d_model 96
+    --d_model 96 \
+    --attn_dropout $attn_dropout \
+    --hidden_dim $hidden_dim \
+    --mlp_ratio $mlp_ratio \
+    --n_depth $n_depth \
+    --n_emb $n_emb \
+    --n_heads $n_heads \
+    --timesteps $timesteps 
+done
+
+# ETTh1
+
+# for rate in 0.125 0.25 0.375 0.5
+# do
+#   python -u run.py \
+#     --task_name imputation \
+#     --is_training 1 \
+#     --root_path ./dataset/ETT-small/ \
+#     --data_path ETTh1.csv \
+#     --model_id ETTh1_mask_$rate \
+#     --mask_rate $rate \
+#     --model $model_name \
+#     --data custom \
+#     --features M \
+#     --pred_len 96 \
+#     --c_out 7 \
+#     --gpu $device \
+#     --d_model 96 \
 #     --attn_dropout $attn_dropout \
 #     --hidden_dim $hidden_dim \
 #     --mlp_ratio $mlp_ratio \
@@ -95,7 +95,7 @@ do
 #     --n_emb $n_emb \
 #     --n_heads $n_heads \
 #     --timesteps $timesteps 
-done
+# done
 
 # # ETTh2
 # for rate in 0.125 0.25 0.375 0.5
@@ -150,28 +150,28 @@ done
 # done
 
 
-# # ETTm2
-# for rate in 0.125 0.25 0.375 0.5
-# do
-#   python -u run.py \
-#     --task_name imputation \
-#     --is_training 1 \
-#     --root_path ./dataset/ETT-small/ \
-#     --data_path ETTm2.csv \
-#     --model_id ETTm2_mask_$rate \
-#     --mask_rate $rate \
-#     --model $model_name \
-#     --data custom \
-#     --features M \
-#     --pred_len 96 \
-#     --c_out 7 \
-#     --gpu $device \
-#     --d_model 96 \
-#     --attn_dropout $attn_dropout \
-#     --hidden_dim $hidden_dim \
-#     --mlp_ratio $mlp_ratio \
-#     --n_depth $n_depth \
-#     --n_emb $n_emb \
-#     --n_heads $n_heads \
-#     --timesteps $timesteps 
-# done
+# ETTm2
+for rate in 0.125 0.25 0.375 0.5
+do
+  python -u run.py \
+    --task_name imputation \
+    --is_training 1 \
+    --root_path ./dataset/ETT-small/ \
+    --data_path ETTm2.csv \
+    --model_id ETTm2_mask_$rate \
+    --mask_rate $rate \
+    --model $model_name \
+    --data custom \
+    --features M \
+    --pred_len 96 \
+    --c_out 7 \
+    --gpu $device \
+    --d_model 96 \
+    --attn_dropout $attn_dropout \
+    --hidden_dim $hidden_dim \
+    --mlp_ratio $mlp_ratio \
+    --n_depth $n_depth \
+    --n_emb $n_emb \
+    --n_heads $n_heads \
+    --timesteps $timesteps 
+done
