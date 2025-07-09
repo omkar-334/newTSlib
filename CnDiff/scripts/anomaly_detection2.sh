dd90007d61878b6dd645a7951162dcbd010b94d7
Omkar Kabde <omkarkabde@gmail.com>
2025-07-09 14:09:04 +0530

    scripts push

--- CnDiff/scripts/anomaly_detection2.sh
+++ CnDiff/scripts/anomaly_detection2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 model_name="CnDiff"
-description="run"
+description="try2"
 cond=1
 tphi=2
 
-attn_dropout=0.3
+attn_dropout=0.0
 mlp_ratio=1
-n_depth=2
-n_emb=4
-n_heads=16
-timesteps=100
+n_depth=1
+n_emb=8
+n_heads=4
+timesteps=200
 
 for tphi_loss in True False
 do
@@ -19,31 +19,31 @@ do
     for hidden_dim in 256 512 768
     do
       python -u run.py \
-        --task_name anomaly_detection \
-        --is_training 1 \
-        --root_path ./dataset/MSL \
-        --model_id MSL \
-        --model $model_name \
-        --data MSL \
-        --wandb False \
-        --features M \
-        --pred_len 96 \
-        --d_model 96 \
-        --c_out 55 \
-        --anomaly_ratio 1 \
-        --batch_size 128 \
-        --train_epochs 10 \
-        --use_cond $cond \
-        --use_tphi $tphi \
-        --attn_dropout $attn_dropout \
-        --hidden_dim $hidden_dim \
-        --mlp_ratio $mlp_ratio \
-        --n_depth $n_depth \
-        --n_emb $n_emb \
-        --n_heads $n_heads \
-        --timesteps $timesteps \
-        --tphi_loss $tphi_loss \
-        --normalize $normalize
+      --task_name anomaly_detection \
+      --is_training 1 \
+      --root_path ./dataset/SMD \
+      --model_id SMD \
+      --model $model_name \
+      --data SMD \
+      --wandb False \
+      --features M \
+      --pred_len 96 \
+      --d_model 96 \
+      --c_out 38 \
+      --anomaly_ratio 0.5 \
+      --batch_size 128 \
+      --train_epochs 10 \
+      --use_cond $cond \
+      --use_tphi $tphi \
+      --attn_dropout $attn_dropout \
+      --hidden_dim $hidden_dim \
+      --mlp_ratio $mlp_ratio \
+      --n_depth $n_depth \
+      --n_emb $n_emb \
+      --n_heads $n_heads \
+      --timesteps $timesteps \
+      --tphi_loss $tphi_loss \
+      --normalize $normalize
     done
   done
 done
--- CnDiff/scripts/imputation.sh
+++ CnDiff/scripts/imputation.sh
@@ -1,7 +1,7 @@
 model_name=CnDiff
 device=1
 cond=1
-tphi=True
+tphi=2
 
 attn_dropout=0.1
 # 0.1
@@ -19,6 +19,7 @@ timesteps=100
 # 100
 
 
+# # ECL
 # for rate in 0.125 0.25 0.375 0.5
 # do
 #   python -u run.py \
@@ -35,6 +36,7 @@ timesteps=100
 #     --pred_len 96 \
 #     --c_out 321 \
 #     --d_model 96 \
+#     --use_tphi $tphi \
 #     --attn_dropout $attn_dropout \
 #     --hidden_dim $hidden_dim \
 #     --mlp_ratio $mlp_ratio \
@@ -44,7 +46,7 @@ timesteps=100
 #     --timesteps $timesteps 
 # done
 
-
+# # Weather
 # for rate in 0.125 0.25 0.375 0.5
 # do
 #   python -u run.py \
@@ -61,6 +63,7 @@ timesteps=100
 #     --c_out 21 \
 #     --gpu $device \
 #     --d_model 96 \
+#     --use_tphi $tphi \
 #     --attn_dropout $attn_dropout \
 #     --hidden_dim $hidden_dim \
 #     --mlp_ratio $mlp_ratio \
@@ -71,7 +74,6 @@ timesteps=100
 # done
 
 # ETTh1
-
 for rate in 0.125 0.25 0.375 0.5
 do
   python -u run.py \
@@ -88,6 +90,7 @@ do
     --c_out 7 \
     --gpu $device \
     --d_model 96 \
+    --use_tphi $tphi \
     --attn_dropout $attn_dropout \
     --hidden_dim $hidden_dim \
     --mlp_ratio $mlp_ratio \
@@ -97,81 +100,84 @@ do
     --timesteps $timesteps
 done
 
-# # ETTh2
-# for rate in 0.125 0.25 0.375 0.5
-# do
-#   python -u run.py \
-#     --task_name imputation \
-#     --is_training 1 \
-#     --root_path ./dataset/ETT-small/ \
-#     --data_path ETTh2.csv \
-#     --model_id ETTh2_mask_$rate \
-#     --mask_rate $rate \
-#     --model $model_name \
-#     --data custom \
-#     --features M \
-#     --pred_len 96 \
-#     --c_out 7 \
-#     --gpu $device \
-#     --d_model 96 \
-#     --attn_dropout $attn_dropout \
-#     --hidden_dim $hidden_dim \
-#     --mlp_ratio $mlp_ratio \
-#     --n_depth $n_depth \
-#     --n_emb $n_emb \
-#     --n_heads $n_heads \
-#     --timesteps $timesteps 
-# done
+# ETTh2
+for rate in 0.125 0.25 0.375 0.5
+do
+  python -u run.py \
+    --task_name imputation \
+    --is_training 1 \
+    --root_path ./dataset/ETT-small/ \
+    --data_path ETTh2.csv \
+    --model_id ETTh2_mask_$rate \
+    --mask_rate $rate \
+    --model $model_name \
+    --data custom \
+    --features M \
+    --pred_len 96 \
+    --c_out 7 \
+    --gpu $device \
+    --d_model 96 \
+    --use_tphi $tphi \
+    --attn_dropout $attn_dropout \
+    --hidden_dim $hidden_dim \
+    --mlp_ratio $mlp_ratio \
+    --n_depth $n_depth \
+    --n_emb $n_emb \
+    --n_heads $n_heads \
+    --timesteps $timesteps 
+done
 
-# # ETTm1
-# for rate in 0.125 0.25 0.375 0.5
-# do
-#   python -u run.py \
-#     --task_name imputation \
-#     --is_training 1 \
-#     --root_path ./dataset/ETT-small/ \
-#     --data_path ETTm1.csv \
-#     --model_id ETTm1_mask_$rate \
-#     --mask_rate $rate \
-#     --model $model_name \
-#     --data custom \
-#     --features M \
-#     --pred_len 96 \
-#     --c_out 7 \
-#     --gpu $device \
-#     --d_model 96 \
-#     --attn_dropout $attn_dropout \
-#     --hidden_dim $hidden_dim \
-#     --mlp_ratio $mlp_ratio \
-#     --n_depth $n_depth \
-#     --n_emb $n_emb \
-#     --n_heads $n_heads \
-#     --timesteps $timesteps 
-# done
+# ETTm1
+for rate in 0.125 0.25 0.375 0.5
+do
+  python -u run.py \
+    --task_name imputation \
+    --is_training 1 \
+    --root_path ./dataset/ETT-small/ \
+    --data_path ETTm1.csv \
+    --model_id ETTm1_mask_$rate \
+    --mask_rate $rate \
+    --model $model_name \
+    --data custom \
+    --features M \
+    --pred_len 96 \
+    --c_out 7 \
+    --gpu $device \
+    --d_model 96 \
+    --use_tphi $tphi \
+    --attn_dropout $attn_dropout \
+    --hidden_dim $hidden_dim \
+    --mlp_ratio $mlp_ratio \
+    --n_depth $n_depth \
+    --n_emb $n_emb \
+    --n_heads $n_heads \
+    --timesteps $timesteps 
+done
 
 
 # ETTm2
-# for rate in 0.125 0.25 0.375 0.5
-# do
-#   python -u run.py \
-#     --task_name imputation \
-#     --is_training 1 \
-#     --root_path ./dataset/ETT-small/ \
-#     --data_path ETTm2.csv \
-#     --model_id ETTm2_mask_$rate \
-#     --mask_rate $rate \
-#     --model $model_name \
-#     --data custom \
-#     --features M \
-#     --pred_len 96 \
-#     --c_out 7 \
-#     --gpu $device \
-#     --d_model 96 \
-#     --attn_dropout $attn_dropout \
-#     --hidden_dim $hidden_dim \
-#     --mlp_ratio $mlp_ratio \
-#     --n_depth $n_depth \
-#     --n_emb $n_emb \
-#     --n_heads $n_heads \
-#     --timesteps $timesteps 
-# done
+for rate in 0.125 0.25 0.375 0.5
+do
+  python -u run.py \
+    --task_name imputation \
+    --is_training 1 \
+    --root_path ./dataset/ETT-small/ \
+    --data_path ETTm2.csv \
+    --model_id ETTm2_mask_$rate \
+    --mask_rate $rate \
+    --model $model_name \
+    --data custom \
+    --features M \
+    --pred_len 96 \
+    --c_out 7 \
+    --gpu $device \
+    --d_model 96 \
+    --use_tphi $tphi \
+    --attn_dropout $attn_dropout \
+    --hidden_dim $hidden_dim \
+    --mlp_ratio $mlp_ratio \
+    --n_depth $n_depth \
+    --n_emb $n_emb \
+    --n_heads $n_heads \
+    --timesteps $timesteps 
+done
