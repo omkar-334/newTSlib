c1ede0567dd96e24d3edf4a341f37014cdd3bbb1
Omkar Kabde <omkarkabde@gmail.com>
2025-07-14 12:55:22 +0530

    missing AD scripts

---
+++ scripts/anomaly_detection/Informer.sh
@@ -0,0 +1,98 @@
+
+# python -u run.py \
+#   --task_name anomaly_detection \
+#   --is_training 1 \
+#   --root_path ./dataset/MSL \
+#   --model_id MSL \
+#   --model Informer \
+#   --data MSL \
+#   --features M \
+#   --seq_len 100 \
+#   --pred_len 0 \
+#   --d_model 128 \
+#   --d_ff 128 \
+#   --e_layers 3 \
+#   --enc_in 55 \
+#   --c_out 55 \
+#   --anomaly_ratio 1 \
+#   --batch_size 128 \
+#   --train_epochs 10
+
+
+python -u run.py \
+  --task_name anomaly_detection \
+  --is_training 1 \
+  --root_path ./dataset/PSM \
+  --model_id PSM \
+  --model Informer \
+  --data PSM \
+  --features M \
+  --seq_len 100 \
+  --pred_len 0 \
+  --d_model 128 \
+  --d_ff 128 \
+  --e_layers 3 \
+  --enc_in 25 \
+  --c_out 25 \
+  --anomaly_ratio 1 \
+  --batch_size 128 \
+  --train_epochs 3
+
+
+python -u run.py \
+  --task_name anomaly_detection \
+  --is_training 1 \
+  --root_path ./dataset/SMAP \
+  --model_id SMAP \
+  --model Informer \
+  --data SMAP \
+  --features M \
+  --seq_len 100 \
+  --pred_len 0 \
+  --d_model 128 \
+  --d_ff 128 \
+  --e_layers 3 \
+  --enc_in 25 \
+  --c_out 25 \
+  --anomaly_ratio 1 \
+  --batch_size 128 \
+  --train_epochs 3
+
+python -u run.py \
+  --task_name anomaly_detection \
+  --is_training 1 \
+  --root_path ./dataset/SMD \
+  --model_id SMD \
+  --model Informer \
+  --data SMD \
+  --features M \
+  --seq_len 100 \
+  --pred_len 0 \
+  --d_model 128 \
+  --d_ff 128 \
+  --e_layers 3 \
+  --enc_in 38 \
+  --c_out 38 \
+  --anomaly_ratio 0.5 \
+  --batch_size 128 \
+  --train_epochs 10
+
+python -u run.py \
+  --task_name anomaly_detection \
+  --is_training 1 \
+  --root_path ./dataset/SWaT \
+  --model_id SWaT \
+  --model Informer \
+  --data SWaT \
+  --features M \
+  --seq_len 100 \
+  --pred_len 0 \
+  --d_model 128 \
+  --d_ff 128 \
+  --e_layers 3 \
+  --enc_in 51 \
+  --c_out 51 \
+  --top_k 3 \
+  --anomaly_ratio 1 \
+  --batch_size 128 \
+  --train_epochs 3
---
+++ scripts/anomaly_detection/MICN.sh
@@ -0,0 +1,98 @@
+
+# python -u run.py \
+#   --task_name anomaly_detection \
+#   --is_training 1 \
+#   --root_path ./dataset/MSL \
+#   --model_id MSL \
+#   --model MICN \
+#   --data MSL \
+#   --features M \
+#   --seq_len 100 \
+#   --pred_len 0 \
+#   --d_model 128 \
+#   --d_ff 128 \
+#   --e_layers 3 \
+#   --enc_in 55 \
+#   --c_out 55 \
+#   --anomaly_ratio 1 \
+#   --batch_size 128 \
+#   --train_epochs 10
+
+
+python -u run.py \
+  --task_name anomaly_detection \
+  --is_training 1 \
+  --root_path ./dataset/PSM \
+  --model_id PSM \
+  --model MICN \
+  --data PSM \
+  --features M \
+  --seq_len 100 \
+  --pred_len 0 \
+  --d_model 128 \
+  --d_ff 128 \
+  --e_layers 3 \
+  --enc_in 25 \
+  --c_out 25 \
+  --anomaly_ratio 1 \
+  --batch_size 128 \
+  --train_epochs 3
+
+
+python -u run.py \
+  --task_name anomaly_detection \
+  --is_training 1 \
+  --root_path ./dataset/SMAP \
+  --model_id SMAP \
+  --model MICN \
+  --data SMAP \
+  --features M \
+  --seq_len 100 \
+  --pred_len 0 \
+  --d_model 128 \
+  --d_ff 128 \
+  --e_layers 3 \
+  --enc_in 25 \
+  --c_out 25 \
+  --anomaly_ratio 1 \
+  --batch_size 128 \
+  --train_epochs 3
+
+python -u run.py \
+  --task_name anomaly_detection \
+  --is_training 1 \
+  --root_path ./dataset/SMD \
+  --model_id SMD \
+  --model MICN \
+  --data SMD \
+  --features M \
+  --seq_len 100 \
+  --pred_len 0 \
+  --d_model 128 \
+  --d_ff 128 \
+  --e_layers 3 \
+  --enc_in 38 \
+  --c_out 38 \
+  --anomaly_ratio 0.5 \
+  --batch_size 128 \
+  --train_epochs 10
+
+python -u run.py \
+  --task_name anomaly_detection \
+  --is_training 1 \
+  --root_path ./dataset/SWaT \
+  --model_id SWaT \
+  --model MICN \
+  --data SWaT \
+  --features M \
+  --seq_len 100 \
+  --pred_len 0 \
+  --d_model 128 \
+  --d_ff 128 \
+  --e_layers 3 \
+  --enc_in 51 \
+  --c_out 51 \
+  --top_k 3 \
+  --anomaly_ratio 1 \
+  --batch_size 128 \
+  --train_epochs 3
